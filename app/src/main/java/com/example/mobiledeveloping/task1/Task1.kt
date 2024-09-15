929d7b27a2ecf9c3145a134b2041c57fa16b7e67
Илья Шаров <s.ilyaharov232@gmail.com>
2024-09-15 22:38:32 +0300

    [fix] Задание 1

--- app/src/main/java/com/example/mobiledeveloping/task1/Task1.kt
+++ app/src/main/java/com/example/mobiledeveloping/task1/Task1.kt
@@ -1,2 +1,17 @@
 package com.example.mobiledeveloping.task1
 
+fun main(){
+    task1()
+}
+
+fun task1(){
+    var number: Int? = null
+    //null
+    println(number)
+
+    number = 12
+    val text: String = number.toString()
+    //12
+    println(text)
+}
+
